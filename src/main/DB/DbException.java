6100cbf512875bb6de3fa63975e395b8b5a4eb9f
José Anderson <joseandersonsilv4@gmail.com>
2023-03-20 15:37:09 -0300

    att

--- src/main/DB/DbException.java
+++
@@ -1,9 +0,0 @@
-package com.hargclinical.harg.DB;
-
-public class DbException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public DbException(String msg) {
-		super(msg);
-	}
-}
