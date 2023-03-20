16d5f3569eef58300172c773b0d905492665c97d
José Anderson <joseandersonsilv4@gmail.com>
2023-03-20 15:30:30 -0300

    att teste com banco

--- src/main/java/com/hargclinical/harg/appTest.java
+++ src/main/java/com/hargclinical/harg/appTest.java
@@ -1,14 +1,28 @@
 package com.hargclinical.harg;
 
-import java.sql.Connection;
+import com.hargclinical.harg.entities.Pessoa;
 
-import com.hargclinical.harg.DB.DB;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class appTest {
 
 	public static void main(String[] args) {
+		Pessoa p1 = new Pessoa("Anderson", "70850528488", 22, 'M');
+		Pessoa p2 = new Pessoa("Higor", "12548965874", 23, 'M');
+		Pessoa p3 = new Pessoa("Rita", "25687569856", 19, 'F');
+		Pessoa p4 = new Pessoa("Guilherme", "12569854869", 19, 'F');
 
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("harg-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.persist(p4);
+		em.getTransaction().commit();
+		System.out.println("Showwwwwwwwwww--------------------");
+		
 	}
 }
--- src/main/java/com/hargclinical/harg/entities/Pessoa.java
+++ src/main/java/com/hargclinical/harg/entities/Pessoa.java
@@ -2,8 +2,18 @@ package com.hargclinical.harg.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
 public class Pessoa implements Serializable{
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int idade;
     public char sexo;
     public String cpf;
