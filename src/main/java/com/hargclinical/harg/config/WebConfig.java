e3f0d39e790d6793cb80dd8d78af7eb9075b6648
Higor de Lima <limahigor.g@gmail.com>
2023-03-21 23:00:27 -0300

    update

--- src/main/java/com/hargclinical/harg/config/WebConfig.java
+++ src/main/java/com/hargclinical/harg/config/WebConfig.java
@@ -10,13 +10,16 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("forward:/html/index.html");
-        registry.addViewController("/cadastrar-paciente").setViewName("forward:/html/cadastrar-paciente.html");
-        registry.addViewController("/cadastrar-medico").setViewName("forward:/html/cadastrar-medico.html");
-        registry.addViewController("/marcar-consulta").setViewName("forward:/html/index.html");
-        /*registry.addViewController("/cadastrar-medico").setViewName("forward:/cadastrar-medico.html");
-        registry.addViewController("/cadastrar-medico").setViewName("forward:/cadastrar-medico.html");
-        registry.addViewController("/cadastrar-medico").setViewName("forward:/cadastrar-medico.html");
-        registry.addViewController("/cadastrar-medico").setViewName("forward:/cadastrar-medico.html");*/
+        registry.addViewController("/cadastrar-paciente").setViewName("forward:/html/cadastrar/cadastrar-paciente.html");
+        registry.addViewController("/cadastrar-medico").setViewName("forward:/html/cadastrar/cadastrar-medico.html");
+        registry.addViewController("/marcar-consulta").setViewName("forward:/html/marcar/marcar-consulta.html");
+        registry.addViewController("/marcar-exame").setViewName("forward:/html/marcar/marcar-exame.html");
+        registry.addViewController("/marcar-procedimento").setViewName("forward:/html/marcar/marcar-procedimento.html");
+        // registry.addViewController("/buscar-paciente").setViewName("forward:/buscar-paciente.html");
+        // registry.addViewController("/buscar-medico").setViewName("forward:/buscar-medico.html");
+        // registry.addViewController("/cadastrar-medico").setViewName("forward:/cadastrar-medico.html");
+        // registry.addViewController("/cadastrar-medico").setViewName("forward:/cadastrar-medico.html");
+        // registry.addViewController("/cadastrar-medico").setViewName("forward:/cadastrar-medico.html");
     }
 
 }
