b2c80940355b8b6777621b312bf9360b8ffa25d7
ritalemos <rklp@ic.ufal.br>
2023-03-23 10:03:09 -0300

    att

--- src/main/java/com/hargclinical/harg/entities/Paciente.java
+++ src/main/java/com/hargclinical/harg/entities/Paciente.java
@@ -1,18 +1,23 @@
 package com.hargclinical.harg.entities;
 
+import com.hargclinical.harg.entities_enums.Plano;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="pacientes")
 public class Paciente extends Pessoa{
+    
+    public Plano plano_saude;
 
     public Paciente(){
         super();
     }
 
-    public Paciente(String nome, String cpf, int idade, char sexo){
+    public Paciente(String nome, String cpf, int idade, char sexo, Plano plano_saude){
         super(nome, cpf, idade, sexo); 
+        this.plano_saude = plano_saude;
     }
     
 }
