b1377afe2e7a71aad0787adc6622943be6caffc9
Guilherme Gomes <107087514+Kaizeraaa@users.noreply.github.com>
2023-04-05 00:23:36 -0300

    att Entitie Caixa e Service CaixaService

--- src/main/java/com/hargclinical/harg/entities/Caixa.java
+++ src/main/java/com/hargclinical/harg/entities/Caixa.java
@@ -1,6 +1,8 @@
 package com.hargclinical.harg.entities;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +30,8 @@ public class Caixa implements Serializable{
 
     private Double saldo;
     private boolean aberto;
+    private LocalDate data;
+    private LocalTime hora;
 
     @OneToMany(mappedBy = "caixa", cascade = CascadeType.ALL)
     private List<Orcamento> movimentacoes = new ArrayList<>();
@@ -40,6 +44,7 @@ public class Caixa implements Serializable{
     public Double getSaldo() {
         return saldo;
     }
+
     public void setSaldo(Double saldo) {
         this.saldo = saldo;
     }
@@ -47,8 +52,35 @@ public class Caixa implements Serializable{
     public boolean getAberto() {
         return aberto;
     }
-    public void setAberto(boolean aberto) {
+
+    public void setAberto(boolean aberto, LocalDate data, LocalTime hora) {
+        this.data = data;
+        this.hora = hora;
         this.aberto = aberto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+    
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
     }
 
     public List<Orcamento> getMovimentacoes() {
@@ -59,7 +91,29 @@ public class Caixa implements Serializable{
         movimentacoes.add(orcamento);
     }
 
-    public Long getId() {
-        return id;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Caixa other = (Caixa) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+    
 }
