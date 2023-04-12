b19892cbc8648fee58468f7b57d744715713b6e3
Guilherme Gomes <107087514+Kaizeraaa@users.noreply.github.com>
2023-04-11 22:28:05 -0300

    att

--- src/main/java/com/hargclinical/harg/services/exceptions/IllegalArgument.java
+++ src/main/java/com/hargclinical/harg/services/exceptions/IllegalArgument.java
@@ -2,6 +2,6 @@ package com.hargclinical.harg.services.exceptions;
 
 public class IllegalArgument extends RuntimeException {
     public IllegalArgument(String msg) {
-        super("Invalid argument." + msg);
+        super(msg);
     }
 }
